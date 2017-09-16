323503507caf66b9d55f5a8ffdd9265200aa314c
GOWRI S <31369935+GOWRICSE@users.noreply.github.com>
2017-09-16 14:33:57 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,13 @@
+package larg;
+import java.*;
+public class Sum {
+	public static void main( String arg[])
+	{
+		int i,j,add;
+		i=10;
+		j=20;
+		add=i+j;
+		System.out.println("SUM: "+add);
+		 
+	}
+}
