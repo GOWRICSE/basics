8056dd5dca0a1c40893c2a9e51d7c737f4b35287
GOWRI S <31369935+GOWRICSE@users.noreply.github.com>
2017-09-30 13:12:45 +0530

    Add files via upload

---
+++ StrInt.java
@@ -0,0 +1,15 @@
+package larg;
+import java.util.*;
+public class StrInt {
+	private static Scanner sc;
+	public static void main(String args[] )
+	{
+		sc=new Scanner(System.in);
+		System.out.println("enter the string : ");
+		String a=sc.next();
+		int i=Integer.parseInt(a);
+		System.out.println("Integer value is "+i);
+		
+	}
+
+}
